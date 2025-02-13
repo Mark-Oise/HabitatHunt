cdb39af5de7ec1653aa7910639fd54f779ce25a9
TheTiredOne22 <120611653+TheTiredOne22@users.noreply.github.com>
2025-02-13 08:37:41 +0100

    Implement active search functionality for leads

--- apps/leads/utils.py
+++ apps/leads/utils.py
@@ -5,13 +5,16 @@ from django.shortcuts import render
 
 def search_leads(request):
     search_query = request.GET.get('search', '').strip()
-    leads_list = Lead.objects.filter(user=request.user)
+    leads = Lead.objects.filter(user=request.user)
 
     if search_query:
-        leads_list = leads_list.filter(
+        leads = leads.filter(
             Q(name__icontains=search_query) |
-            Q(username__icontains=search_query) |
-            Q(email__icontains=search_query)
+            Q(username__icontains=search_query) 
+            # Q(email__icontains=search_query)
         )
 
-    return render(request, 'dashboard/components/leads/lead_items.html', {'leads_list': leads_list})
+    return render(request, 'dashboard/components/leads/lead_items.html', {
+        'leads': leads,
+        'search_query': search_query
+    })
