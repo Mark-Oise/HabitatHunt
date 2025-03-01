ef11c04712e5db03c7f9204330751f38f4767336
TheTiredOne22 <120611653+TheTiredOne22@users.noreply.github.com>
2025-03-01 17:53:04 +0100

    Hashtags view

--- apps/hashtags/views.py
+++ apps/hashtags/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .forms import HashtagForm
+from .models import Hashtag
 
 # Create your views here.
+def hashtags(request):
+    if request.method == 'POST':
+        form = HashtagForm()
+        if form.is_valid():
+            form.save()
+
+    hashtags = Hashtag.objects.filter(user=request.user)
+
+    context = {
+        'hashtags': hashtags,
+        'form': form,
+    }
+
+
+    return render(request, 'hashtags/hashtags.html', context)
