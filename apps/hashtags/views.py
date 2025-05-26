6abde40891a7733d25c836f94ee2d835b89cf781
TheTiredOne22 <120611653+TheTiredOne22@users.noreply.github.com>
2025-05-26 13:56:22 +0100

    Fix UnboundLocalError: local variable 'paginated_hashtags' referenced before assignment

--- apps/hashtags/views.py
+++ apps/hashtags/views.py
@@ -1,20 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .forms import HashtagForm
 from .models import Hashtag
 from apps.platforms.models import Platform
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.shortcuts import get_object_or_404
-
 
 # Helper function to get paginated hashtags
 def get_paginated_hashtags(request):
-    # Query hashtags with ordering
     hashtags = Hashtag.objects.filter(user=request.user).order_by('-created_at')
-    
-    # Handle pagination
     page = request.GET.get('page', 1)
     paginator = Paginator(hashtags, 10)  # Show 10 hashtags per page
-    
     try:
         return paginator.page(page)
     except PageNotAnInteger:
@@ -22,75 +16,54 @@ def get_paginated_hashtags(request):
     except EmptyPage:
         return paginator.page(paginator.num_pages)
 
-
-# Create your views here.
-def hashtags(request): 
+def hashtags(request):
+    paginated_hashtags = None 
     if request.method == 'POST':
         form = HashtagForm(request.POST)
         if form.is_valid():
             hashtag = form.save(commit=False)
             hashtag.user = request.user
             hashtag.save()
-            # Get updated hashtags after adding new one
             paginated_hashtags = get_paginated_hashtags(request)
-
+        else:
+            paginated_hashtags = get_paginated_hashtags(request)
         return render(request, 'hashtags/components/hashtag_items.html', {
-                'hashtags': paginated_hashtags,
-                'form': form, 
-                'platforms': Platform.objects.filter(is_active=True)})
-
+            'hashtags': paginated_hashtags,
+            'form': form,
+            'platforms': Platform.objects.filter(is_active=True)
+        })
 
     total_hashtags = Hashtag.objects.filter(user=request.user).count()
     form = HashtagForm()
     paginated_hashtags = get_paginated_hashtags(request)
-
     context = {
         'hashtags': paginated_hashtags,
         'total_hashtags': total_hashtags,
         'form': form,
         'platforms': Platform.objects.filter(is_active=True),
     }
-
     return render(request, 'hashtags/hashtags.html', context)
-
-
-
-
 
 def bulk_delete_hashtags(request):
     delete_all = request.GET.get('delete_all') == 'true'
-    
     if delete_all:
-        # Delete all targets for the current user
         Hashtag.objects.filter(user=request.user).delete()
     else:
-        # Delete selected targets
         selected_hashtags = request.POST.getlist('selected_hashtags')
         Hashtag.objects.filter(
             id__in=selected_hashtags,
             user=request.user
         ).delete()
-
-    hashtags = Hashtag.objects.filter(user=request.user).order_by('-created_at')
-
-    # Get updated hashtags list with pagination
     paginated_hashtags = get_paginated_hashtags(request)
-
     return render(request, 'hashtags/components/hashtag_items.html', {
-            'hashtags': paginated_hashtags,
-        })
-
-
-
+        'hashtags': paginated_hashtags,
+    })
 
 def delete_hashtag(request, hashtag_id):
     if request.method == 'POST':
-        hashtags = get_object_or_404(Hashtag, id=hashtag_id, user=request.user)
-        hashtags.delete()
-        
-        # Get updated hashtags list with pagination
+        hashtag = get_object_or_404(Hashtag, id=hashtag_id, user=request.user)
+        hashtag.delete()
         paginated_hashtags = get_paginated_hashtags(request)
-            
         return render(request, 'hashtags/components/hashtag_items.html', {
             'hashtags': paginated_hashtags,
         })
