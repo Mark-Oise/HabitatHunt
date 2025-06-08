d2daf8bbf5089fb612d6a693e22fa9ede2f547a6
TheTiredOne22 <120611653+TheTiredOne22@users.noreply.github.com>
2025-06-08 12:11:54 +0100

    Custom location form and crud functions

---
+++ apps/locations/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import CustomLocation
+
+class CustomLocationForm(forms.ModelForm):
+    class Meta:
+        model = CustomLocation
+        fields = ['location_text']
--- apps/locations/views.py
+++ apps/locations/views.py
@@ -5,3 +5,22 @@ from .models import Province  # Add this import
 def locations(request):
     provinces = Province.objects.all().select_related('country')  # Get all provinces with country data
     return render(request, 'locations/locations.html', {'provinces': provinces})
+
+
+
+def add_custom_location(request):
+    pass
+
+def delete_custom_location(request, location_id):
+    pass
+
+def update_custom_location(request, location_id):
+    pass
+
+
+def search_custom_location(request, location_id):
+    pass
+
+
+
+
